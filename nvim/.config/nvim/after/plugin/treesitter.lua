f16212a79d69545e2b29187552f25d315f65218d
Tommy Guo <tommyguo024@outlook.com>
2026-01-04 01:38:58 -0500

    chore: add svelte treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter'.setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "python", "ruby", "html", "css" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "python", "ruby", "html", "css", "svelte" },
   sync_install = false,
   auto_install = true,
   highlight = {
