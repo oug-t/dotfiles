2cbce2cb30c1eebf91f03486c6a1fd160513b521
Tommy Guo <tommyguo024@outlook.com>
2026-03-13 10:42:32 -0400

    chore(nvim): refine oil navigation

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -1,13 +1,7 @@
 vim.g.mapleader = " "
 
 -- Oil file manager
-vim.keymap.set("n", "-", function()
-    require("oil").open()
-end, { desc = "Open Oil File Manager" })
-
-vim.keymap.set("n", "<leader>pv", function()
-    require("oil").open()
-end, { desc = "Open Oil File Manager" })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
