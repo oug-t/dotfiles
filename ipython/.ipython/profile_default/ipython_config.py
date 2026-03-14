4ab58d17bf356158165269face76017591db42f2
Tommy Guo <tommyguo024@outlook.com>
2026-03-14 00:57:28 -0400

    fix(ipython): address highlighting_style warning

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -3,6 +3,6 @@ c = get_config()
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.confirm_exit = False
 
-c.TerminalInteractiveShell.highlighting_style = "nord"
+c.TerminalInteractiveShell.theme = "nord"
 c.TerminalInteractiveShell.prompts_class = "IPython.terminal.prompts.ClassicPrompts"
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
