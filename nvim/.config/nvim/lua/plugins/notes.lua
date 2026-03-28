7a60b91395762e26328d6b7c8a516ad5b4a66d72
oug-t <tommyguo024@outlook.com>
2026-03-28 16:38:06 -0400

    chore(nvim): clean unused notes plugins

--- nvim/.config/nvim/lua/plugins/notes.lua
+++ nvim/.config/nvim/lua/plugins/notes.lua
@@ -1,46 +1,5 @@
 return {
     {
-        "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons", 'nvim-mini/mini.nvim', 'nvim-mini/mini.icons' },
-        ft = { "markdown" },
-        opts = {
-            enabled = true,
-            render_modes = { "n", "c", "t", "i" },
-            win_config = {
-                concealcursor = "nc",
-            },
-            latex = {
-                enabled = true,
-                converter = 'utftex',
-            },
-            html = { enabled = false },
-            yaml = { enabled = false },
-            win_options = {
-                conceallevel = { default = vim.o.conceallevel, rendered = 2 },
-            },
-        },
-    },
-    {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-            default = {
-                dir_path = "assets",
-            },
-        },
-        keys = {
-            { "<leader>pi", "<cmd>PasteImage<cr>", desc = "Paste image" },
-        },
-    },
-    {
-        "dhruvasagar/vim-table-mode",
-        ft = { "markdown", "org" },
-        cmd = "TableModeToggle",
-        keys = {
-            { "<leader>tm", "<cmd>TableModeToggle<cr>", desc = "Toggle Table Mode" },
-        },
-    },
-    {
         "lervag/vimtex",
         lazy = false,
         init = function()
