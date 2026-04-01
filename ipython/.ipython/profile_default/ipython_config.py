dbab7861133bd6cac3b3a9bb3ae51e5f62ab3ff1
Xiyuan Guo <tommyguo039@gmail.com>
2026-03-31 21:43:17 -0400

    chore(ipython): kill syntax highlighting

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -1,7 +1,9 @@
 c = get_config()
 
+c.InteractiveShell.colors = 'NoColor'
+
+c.TerminalInteractiveShell.highlighting_style = "legacy"
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.confirm_exit = False
-
 c.TerminalInteractiveShell.prompts_class = "IPython.terminal.prompts.ClassicPrompts"
